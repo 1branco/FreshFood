7c05c329313b5493067955c5fa7c5d7eff24b28d
1branco <joao97branco@gmail.com>
2023-12-31 00:51:42 +0000

    initial commit
    created frontend project and API gateway project

---
+++ FreshFood/APIGateway/Program.cs
@@ -0,0 +1,43 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            // 1. add Configuration
+            builder.Configuration.AddJsonFile("ocelot.json", false, true);
+            builder.Services.AddOcelot();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            await app.UseOcelot();
+
+            app.Run();
+        }
+    }
+}
