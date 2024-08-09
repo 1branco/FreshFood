090c38f934d38ad756d9abcff8c5d30afa5fc153
1branco <joao97branco@gmail.com>
2024-08-09 17:32:34 +0100

    Deleted PerformanceTests project, FFD-5

--- FreshFood/Tests/PerformanceTests/PerformanceTests/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PerformanceTests;
-
-class Program
-{
-    static void Main(string[] args)
-    {   
-        var summary = BenchmarkRunner.Run<Services.LoginPerformanceTest>();
-    }
-}
